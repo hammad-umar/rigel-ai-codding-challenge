1f11da2d6ec1fab8d1c1f1b0367dacfd3b259947
Hammad Umar <hammadhawc227@gmail.com>
2025-02-23 20:20:30 +0500

    setup proper error handling

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import httpStatus from 'http-status'
+import ApiError from './utils/ApiError'
+import { errorConverter, errorHandler } from './middlewares/error'
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(cors())
 app.options('*', cors())
 
-app.use((_, res, next) => {
-  res.status(httpStatus.NOT_FOUND).json('Not Found')
-  next()
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
 })
+
+app.use(errorConverter)
+app.use(errorHandler)
 
 export default app
